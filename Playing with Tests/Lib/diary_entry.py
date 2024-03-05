493b37f4753427d3f8c1337c13f55c7127988ae0
Ro-0171 <143122137+Ro-0171@users.noreply.github.com>
2024-03-05 21:49:05 +0000

    Add files via upload

---
+++ Playing with Tests/Lib/diary_entry.py
@@ -0,0 +1,35 @@
+class DiaryEntry:
+    def __init__(self, title, contents):
+        if title == "" or contents == "":
+            raise Exception("Diary entry must have a title and contents")
+        self.title = title
+        self.contents = contents
+        self.read_so_far = 0
+
+    def format(self):
+        return f"{self.title}: {self.contents}"
+
+    def count_words(self):
+        words = self.format().split()
+        return len(words)
+
+
+    def reading_time(self, wpm):
+        if wpm == 0:
+            raise Exception("Cannot calclulate reading time with wpm of 0")
+        contents_word_count = len(self.contents.split())
+        return round(contents_word_count / wpm)
+
+    def reading_chunk(self, wpm, minutes):
+        
+        words_user_can_read =  wpm * minutes
+        words = self.contents.split()
+        if self.read_so_far >= len(words):
+            self.read_so_far = 0
+
+        chunk_start = self.read_so_far
+        chunk_end = self.read_so_far + words_user_can_read
+        chunk_words = words[chunk_start:chunk_end]
+        self.read_so_far = chunk_end
+        return " ".join(chunk_words)
+        
